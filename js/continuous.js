2e9b86a307d53e53eb60cd668ce66d1b0f5a43b8
Macbook Pro <macbook@Macbooks-MacBook-Pro-540.local>
2018-10-29 17:18:41 -0400

    added scroll wheel scaling and panning with the mouseX

--- js/continuous.js
+++ js/continuous.js
@@ -1,6 +1,10 @@
 
 let drawing = [];
 let database;
+let scaleAdd = 1;
+let scaleCons = 1;
+let panCons = -20;
+let heightCons = 0;
 
 function setup() {
     createCanvas(2400, windowHeight);
@@ -28,12 +32,16 @@ function setup() {
 	ref.on('value', gotData, errData);
 
     background(30);
-    
+
     setTimeout(drawAllLines, 2000)
+
+
 }
 
 function draw() {
-    // drawAllLines();
+    background(30);
+    drawAllLines();
+
 }
 
 let allDrawingPts = [];
@@ -58,19 +66,24 @@ function errData(err) {
 }
 
 function drawAllLines(){
-    
-    beginShape();
+
+    beginShape(TRIANGLES);
     stroke(230);
     strokeWeight(3);
     noFill();
-    
-    
+
+    // let scale = map(mouseY, 0, height/2, 10, 0);
+
+
+
     for(var i = 0; i<allDrawingPts.length; i++){
         var path = allDrawingPts[i];
         beginShape();
         for(var j = 0; j < path.length; j++){
             for(var j = 0; j < path.length; j++){
-                vertex(path[j].x+i*200, path[j].y);
+                let pan = map(mouseX, 100, width-100, 0, 950*panCons);
+                vertex(path[j].x/scaleCons+(i*950/scaleCons)+pan, path[j].y/scaleCons+heightCons);
+
             }
             endShape();
         }
@@ -84,8 +97,21 @@ function drawAllLines(){
     //     for(let j = 0; j>allDrawingPts[i].length;j++){
 
     //         vertex(allDrawingPts[i][j].x, allDrawingPts[i][j].y);
-            
+
     //     }
     //     endShape();
 	// }
+}
+
+function mouseWheel(event) {
+  //println(event.delta);
+
+  let scaleScroll = map(event.delta, -300, 300, -1, 1);
+  scaleAdd += scaleScroll * - 1;
+  scaleCons = constrain(scaleAdd, 1, 10);
+  panCons = map(scaleCons, 1, 15, -30, 0)
+  heightCons = map(scaleCons, 1, 15, 0, height/1.5)
+  // scaleRev = map(scale, -10, 10, -, -100);
+
+	console.log(scaleCons);
 }
