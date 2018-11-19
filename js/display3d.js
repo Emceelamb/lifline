ca7f5af872d18a07678b9dec67fd901c7fdf8a53
emceelamb <mlam@berkeley.edu>
2018-11-18 19:09:45 -0500

    changed 3d camera display

--- js/display3d.js
+++ js/display3d.js
@@ -1,10 +1,10 @@
-
 let drawing = [];
 let database;
 
 let camX=0, camY=0, camZ=0;
 let xPos = 0, yPos = 0;
 
+let ln;
 
 function setup() {
     createCanvas(windowWidth,windowHeight,WEBGL);
@@ -42,18 +42,18 @@ function setup() {
 
 function draw() {
     cameraControl();
-    camera(camX-500, camY, (height/2.0) / tan(PI*30.0 / 180.0)+camZ+1000, 0, 0, 0, 0, 1, 0);
+    camera(camX, 0, (height/2.0) / tan(PI*30.0 / 180.0)+camZ+1000, ln/2*400+camX, camX, 0, 0, 1, 0);
 
     background(30);
     drawAllLines();
-
 }
 
 let allDrawingPts = [];
 
 function gotData(data) {
 	let drawings = data.val();
-	let keys = Object.keys(drawings);
+    let keys = Object.keys(drawings);
+    ln=keys.length;
 	for (let i=0; i<keys.length; i++) {
 		let key = keys[i];
         // console.log(key);
@@ -71,21 +71,19 @@ function errData(err) {
 }
 
 function drawAllLines(){
-
     beginShape(TRIANGLES);
     stroke(230);
     strokeWeight(3);
     noFill();
-
-
-
+    
+    
+    
     for(var i = 0; i<allDrawingPts.length; i++){
         var path = allDrawingPts[i];
         beginShape();
         for(var j = 0; j < path.length; j++){
             for(var j = 0; j < path.length; j++){
-                vertex(path[j].x, path[j].y,path[j].z);
-
+                vertex(path[j].x, path[j].y,0);
             }
             endShape();
         }
@@ -98,27 +96,27 @@ function drawAllLines(){
 function cameraControl(){
 
     if (keyIsDown(LEFT_ARROW)) {
-        camX-=25;
+        camX-=50;
         console.log(camX, camY, camZ);
     } else if (keyIsDown(RIGHT_ARROW)) {
-        camX+=25;
+        camX+=50;
         console.log(camX, camY, camZ);
 
     } else if (keyIsDown(DOWN_ARROW)){
-        camY+=25;
+        camY+=50;
         console.log(camX, camY, camZ);
 
     } else if (keyIsDown(UP_ARROW)){
-        camY-=25;
+        camY-=50;
         console.log(camX, camY, camZ);
 
     
     } else if (keyIsDown(65)){yPos<400
-        camZ+=25;
+        camZ+=50;
         console.log(camX, camY, camZ);
 
     } else if (keyIsDown(90)){
-        camZ-=25;
+        camZ-=50;
         console.log(camX, camY, camZ);
 
     }
